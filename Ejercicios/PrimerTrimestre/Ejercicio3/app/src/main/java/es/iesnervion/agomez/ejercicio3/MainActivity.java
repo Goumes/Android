d4203f8288c44a1100fad7648a2d4c436deb88af
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-09-27 14:36:32 +0200

    Ejercicio 3 hecho y 4 a medias
    El 3 no funciona bien

--- Ejercicios/PrimerTrimestre/Ejercicio3/app/src/main/java/es/iesnervion/agomez/ejercicio3/MainActivity.java
+++ Ejercicios/PrimerTrimestre/Ejercicio3/app/src/main/java/es/iesnervion/agomez/ejercicio3/MainActivity.java
@@ -2,12 +2,37 @@ package es.iesnervion.agomez.ejercicio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void alinear(View view)
+    {
+        int boton = view.getId();
+        EditText texto = (EditText) findViewById(R.id.texto);
+
+        switch (boton)
+        {
+            case R.id.boton1:
+                texto.setGravity(Gravity.LEFT);
+                break;
+            case R.id.boton2:
+                texto.setGravity(Gravity.END);
+                break;
+        }
     }
 }
