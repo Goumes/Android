2c7b372b947f6a36417ea20c882b7c7afc23eb8a
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-10-02 18:56:16 +0200

    Hecho ejercicio 5

--- Ejercicios/PrimerTrimestre/Ejercicio4/app/src/main/java/es/iesnervion/agomez/ejercicio4/MainActivity.java
+++ Ejercicios/PrimerTrimestre/Ejercicio4/app/src/main/java/es/iesnervion/agomez/ejercicio4/MainActivity.java
@@ -2,6 +2,8 @@ package es.iesnervion.agomez.ejercicio4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void cambiarImagen(View view)
+    {
+        int boton = view.getId();
+        ImageView imagen = (ImageView) findViewById(R.id.imagen);
+
+        switch (boton)
+        {
+            case R.id.boton1:
+                imagen.setImageResource(R.drawable.android4);
+                break;
+            case R.id.boton2:
+                imagen.setImageResource(R.drawable.android6);
+                break;
+        }
     }
 }
---
+++ Ejercicios/PrimerTrimestre/Ejercicio5/app/src/main/java/com/iesnervion/agomez/ejercicio5/MainActivity.java
@@ -0,0 +1,38 @@
+package com.iesnervion.agomez.ejercicio5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    int size = 11;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cambiarTamano(View view)
+    {
+        int id = view.getId();
+        EditText texto = (EditText) findViewById(R.id.texto);
+
+        switch (id)
+        {
+            case R.id.boton1:
+                size--;
+                texto.setTextSize(TypedValue.COMPLEX_UNIT_DIP, size);
+                break;
+
+            case R.id.boton2:
+                size++;
+                texto.setTextSize(TypedValue.COMPLEX_UNIT_DIP, size);
+                break;
+
+        }
+    }
+}
