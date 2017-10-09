411c30212d2d99eccda48ccd7c8c98b1e549719c
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-10-09 12:55:16 +0200

    ARREGLADO
    No preguntes qué fallaba...

--- Ejercicios/PrimerTrimestre/Bol2Ej2/app/src/main/java/es/iesnervion/agomez/bol2ej2/MainActivity.java
+++ Ejercicios/PrimerTrimestre/Bol2Ej2/app/src/main/java/es/iesnervion/agomez/bol2ej2/MainActivity.java
@@ -14,35 +14,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void operacionRadio(View view)
-    {
-        int id = view.getId();
-        EditText num1 = (EditText) findViewById(R.id.num1);
-        EditText num2 = (EditText) findViewById(R.id.num2);
-        TextView resultado = (TextView) findViewById(R.id.resultado);
-        double operacion = 0;
+        public void onClick(View view) {
+            int id = view.getId();
+            EditText num1 = (EditText) findViewById(R.id.num1);
+            EditText num2 = (EditText) findViewById(R.id.num2);
+            TextView resultado = (TextView) findViewById(R.id.resultado);
+            double operacion = 0;
 
-        switch (id)
-        {
-            case R.id.sumar:
-                operacion = (Integer.parseInt(num1.getText().toString()) + Integer.parseInt(num1.getText().toString()));
-                resultado.setText(String.valueOf(operacion));
-                break;
-
-            case R.id.restar:
-                operacion = (Integer.parseInt(num1.getText().toString()) - Integer.parseInt(num1.getText().toString()));
-                resultado.setText(String.valueOf(operacion));
-                break;
-
-            case R.id.multiplicar:
-                operacion = (Integer.parseInt(num1.getText().toString()) * Integer.parseInt(num1.getText().toString()));
-                resultado.setText(String.valueOf(operacion));
-                break;
-
-            case R.id.dividir:
-                operacion = (Integer.parseInt(num1.getText().toString()) / Integer.parseInt(num1.getText().toString()));
-                resultado.setText(String.valueOf(operacion));
-                break;
+            switch (id) {
+                case (R.id.sumar):
+                    operacion = (Double.parseDouble(num1.getText().toString()) + Integer.parseInt(num2.getText().toString()));
+                    resultado.setText(String.valueOf(operacion));
+                    break;
+                case (R.id.restar):
+                    operacion = (Double.parseDouble(num1.getText().toString()) - Integer.parseInt(num2.getText().toString()));
+                    resultado.setText(String.valueOf(operacion));
+                    break;
+                case (R.id.multiplicar):
+                    operacion = (Double.parseDouble(num1.getText().toString()) * Integer.parseInt(num2.getText().toString()));
+                    resultado.setText(String.valueOf(operacion));
+                    break;
+                case (R.id.dividir):
+                    operacion = (Double.parseDouble(num1.getText().toString()) / Integer.parseInt(num2.getText().toString()));
+                    resultado.setText(String.valueOf(operacion));
+                    break;
+            }
         }
     }
-}
