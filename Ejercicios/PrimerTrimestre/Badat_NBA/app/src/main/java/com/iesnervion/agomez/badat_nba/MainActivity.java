435bc4697dfb4ecbfd5559655cb72b01436599a0
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2018-01-10 15:03:48 +0100

    Haciendo la UI del ejercicio 3

---
+++ Ejercicios/PrimerTrimestre/Badat_NBA/app/src/main/java/com/iesnervion/agomez/badat_nba/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iesnervion.agomez.badat_nba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
