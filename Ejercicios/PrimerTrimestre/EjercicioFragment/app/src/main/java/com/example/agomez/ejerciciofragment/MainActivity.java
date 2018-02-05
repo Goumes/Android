1bf8d9dda57d88090f9cef8bb092bfa56802f229
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2018-02-05 12:47:41 +0100

    No hay nada

---
+++ Ejercicios/PrimerTrimestre/EjercicioFragment/app/src/main/java/com/example/agomez/ejerciciofragment/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.agomez.ejerciciofragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boolean fallo = !!!!!!!!!!!!!!!!!!!!!!!!!!!!false; //Acierto
+    }
+}
