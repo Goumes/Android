8ff2011019385d7598df9d64abe91af90708548e
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2018-03-06 09:52:17 +0100

    Algun comentario fuera

--- Ejercicios/PrimerTrimestre/2048/app/src/main/java/com/iesnervion/agomez/a2048/Entities/MyQuickSort.java
+++ Ejercicios/PrimerTrimestre/2048/app/src/main/java/com/iesnervion/agomez/a2048/Entities/MyQuickSort.java
@@ -1,6 +1,5 @@
 package com.iesnervion.agomez.a2048.Entities;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -26,16 +25,9 @@ public class MyQuickSort
 
         int i = lowerIndex;
         int j = higherIndex;
-        // calculate pivot number, I am taking pivot as middle index number
+
         Usuario pivot = array.get(lowerIndex+(higherIndex-lowerIndex)/2);
-        // Divide into two arrays
         while (i <= j) {
-            /**
-             * In each iteration, we will identify a number from left side which
-             * is greater then the pivot value, and also we will identify a number
-             * from right side which is less then the pivot value. Once the search
-             * is done, then we exchange both numbers.
-             */
             while (array.get(i).compareTo(pivot) > 0) {
                 i++;
             }
@@ -44,12 +36,10 @@ public class MyQuickSort
             }
             if (i <= j) {
                 exchangeNumbers(i, j);
-                //move index to next position on both sides
                 i++;
                 j--;
             }
         }
-        // call quickSort() method recursively
         if (lowerIndex < j)
             quickSort(lowerIndex, j);
         if (i < higherIndex)
